184949b92df1b533aff26407a69770ed0dbf04f6
aiman-munawar <aimanmunawar6@gmail.com>
2024-08-29 20:29:48 +0500

    js

---
+++ JavaScript-practice/binary-search.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let start=0;
+    let end=nums.length-1;
+    let mid=Math.floor((start+end)/2);
+    while(start<=end){
+        if(nums[mid]===target){
+            return mid;
+        }
+        else if(nums[mid]>target){
+            end=mid-1;
+        }
+        else if(nums[mid]<target){
+            start=mid+1;
+        }
+        mid=Math.floor((start+end)/2);
+    }
+    return -1;
+};
