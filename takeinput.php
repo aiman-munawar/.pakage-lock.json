559026b4afb6f2613b22cd85245d2d8be47fe8e8
aiman-munawar <aimanmunawar6@gmail.com>
2024-10-11 22:58:23 +0500

    php

---
+++ takeinput.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="create.php" method="POST">
+        <label for="title">Write the title</label>
+        <input type="text" name="title" id="title">
+        <label for="disc">Write the discription</label>
+        <input type="text" name="disc" id="disc">
+        <input type="submit" value="submit" id="submit" name="submit">
+    </form>
+</body>
+</html>
