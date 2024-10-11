906829921ee6240f11696d79c8b651248a0dc2e9
aiman-munawar <aimanmunawar6@gmail.com>
2024-10-11 22:57:53 +0500

    php

---
+++ function.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    function add($a,$b){
+         return $a+$b;
+           }
+    function sub($a,$b){
+         return $a-$b;
+           }
+    function mul($num1,$num2){
+         return $a*$b;
+           }
+    function divd($a,$b){
+         return $a/$b;
+           }
+?>
+</body>
+</html>
